704f7e091aef18ec235f053d2e14c835e8896300
Elena Beccaro <elena.beccaro@gmail.com>
2026-09-10 16:44:30 +0100

    SCRUM-46 configure Prisma ORM-fix

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client.js';
 
 @Injectable()
@@ -6,6 +7,14 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit
 {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
